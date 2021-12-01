639142014f26faaf6ef7142132683db4bfd90641
adamni21 <85580454+adamni21@users.noreply.github.com>
2021-12-01 13:22:24 +0100

    set up apollo client

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,11 +1,19 @@
 import './App.css';
 import { Navigate, Routes } from 'react-router';
 import { BrowserRouter as Router, Route,  } from 'react-router-dom';
+import { ApolloClient, InMemoryCache, ApolloProvider } from "@apollo/client";
+
 import Header from './Components/layout/Header';
 import Home from './Components/pages/Homepage/Home';
 import About from './Components/pages/About/About';
 import Contact from './Components/pages/Contact/Contact';
 import PageNotFound from './Components/pages/404/PageNotFound';
+
+
+const client = new ApolloClient({
+  uri: 'http://localhost:4000/graphql',
+  cache: new InMemoryCache()
+});
 
 export interface blog_entry {
   title: string,
@@ -43,13 +51,15 @@ function App() {
     <Router>
       <div className="App">
         <Header/>
-        <Routes>
-          <Route path="/home" element={ <Home blogEntries={DUMMY_ENTRIES}/> }/>
-          <Route path="/about" element={ <About/> }/>
-          <Route path="/contact" element={ <Contact /> }/>
-          <Route path="/" element={ <Navigate to="/home"/> }/>
-          <Route path="/*" element={ <PageNotFound/> }/>
-        </Routes>
+        <ApolloProvider client={client}>
+          <Routes>
+            <Route path="/home" element={ <Home blogEntries={DUMMY_ENTRIES}/> }/>
+            <Route path="/about" element={ <About/> }/>
+            <Route path="/contact" element={ <Contact /> }/>
+            <Route path="/" element={ <Navigate to="/home"/> }/>
+            <Route path="/*" element={ <PageNotFound/> }/>
+          </Routes>
+        </ApolloProvider>
       </div>
     </Router>
   );
